7382f4bbf39e721b081fb670de9d28692ba25e86
nyakoru <tanweeho@gmail.com>
2025-02-02 19:41:27 +0800

    Able to retrieve href
    Made it so you get all the links so you can directly open the websites to scrape the links

--- teststuf-main/test_scrapper.py
+++ teststuf-main/test_scrapper.py
@@ -6,7 +6,7 @@ import time
 import pandas as pd
 from bs4 import BeautifulSoup
 
-mode = 'extract'
+mode = 'scrape'  ##change accordingly
 
 
 
@@ -15,6 +15,29 @@ if mode == 'scrape':
     s = Service('C:/Users/tanwe/Desktop/scrapper/teststuf-main/tmp/chromedriver/chromedriver.exe') ###Directory of your chromedriver.exe
     driver = webdriver.Chrome(service = s)
     driver.get('https://en.cf-vanguard.com/cardlist/cardsearch/?regulation=D&nation=&clan=&keyword=&keyword_type%5B%5D=all&kind%5B%5D=all&grade%5B%5D=all&power_from=&power_to=&rare=&trigger%5B%5D=all')
+    time.sleep(5)
+    driver.find_element(By.XPATH,"//button[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']").click() #click to pass cookie wall
+    time.sleep(5)
+    previous_height = driver.execute_script('return document.body.scrollHeight')
+
+    while True:
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        time.sleep(5)
+        new_height = driver.execute_script('return document.body.scrollHeight')
+        if new_height == previous_height:
+            break
+        previous_height = new_height
+
+    page_source = driver.page_source
+
+    f = open(path_to_file + "source.txt", "w", encoding= "utf-8")
+    f.write(page_source)
+    f.close()
+
+    ###Scrapping for DivineZ standard regulation
+    s = Service('C:/Users/tanwe/Desktop/scrapper/teststuf-main/tmp/chromedriver/chromedriver.exe') ###Directory of your chromedriver.exe
+    driver = webdriver.Chrome(service = s)
+    driver.get('https://en.cf-vanguard.com/cardlist/cardsearch/?regulation=D&nation=&clan=&keyword=dz&keyword_type%5B%5D=all&kind%5B%5D=all&grade%5B%5D=all&power_from=&power_to=&rare=&trigger%5B%5D=all')
     time.sleep(3)
     driver.find_element(By.XPATH,"//button[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']").click() #click to pass cookie wall
     time.sleep(3)
@@ -29,25 +52,28 @@ if mode == 'scrape':
         previous_height = new_height
 
     page_source = driver.page_source
-
-    f = open(path_to_file + "source.txt", "w", encoding= "utf-8")
+    f = open(path_to_file + "source_z.txt", "w", encoding= "utf-8")
     f.write(page_source)
     f.close()
 
-### Bs4 test
-if mode != 'scrape':
-    data = []
+def scrape_names(f_name):
 
-    f = open(path_to_file + "source.txt", "r", encoding= "utf-8")
+    f = open(f_name, "r", encoding= "utf-8")
     page_source = f.read()
     f.close()
     soup = BeautifulSoup(page_source, features='lxml')
 
-    items = soup.findAll('div', class_ = 'cardlist_gallerylist')
+    items = soup.findAll('a', href = True)
+    all_card_lst = []
 
     for item in items:
         item_out = {}
+        if "/cardlist/?cardno=" in item['href']:
+            all_card_lst.append(item['href'])
+    return all_card_lst
 
-        print(type(items))
-        exit()
-    
+
+### Bs4 test
+if mode != 'scrape':
+    all_d_sets =  scrape_names(path_to_file + "source.txt")
+    all_dz_sets = scrape_names(path_to_file + "source_z.txt")
